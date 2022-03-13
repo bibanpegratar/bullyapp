6fc8e0be33db72861d9c98997db51a80fa65ebf5
biban <turbo_car_fast_speed313@protonmail.com>
2022-03-13 22:30:45 +0200

    debug = True

--- bullyapp/settings.py
+++ bullyapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a!n)vfjv@n%7$e0lb-7)v#^axu0&o#+&ak*yt1ar0khsq%&6af'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'bullyapp.pythonanywhere.com', 'bullyapp.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'bullyapp.pythonanywhere.com']
 
 AUTH_USER_MODEL = 'api.User'
 
